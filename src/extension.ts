6ae33cb01cf74cd5b762455ad07e2bc3f3fde246
Steven Lee <steven.lee@kynrai.com>
2025-05-08 17:24:05 +0100

    format ts

--- src/extension.ts
+++ src/extension.ts
@@ -15,11 +15,11 @@ import {
 let clients: Map<string, LanguageClient> = new Map();
 
 export async function activate() {
-    	if (clients.size > 0) {
-        for (const client of clients.values()) {    
-            await client.stop();
-        }
-        clients.clear();
+	if (clients.size > 0) {
+		for (const client of clients.values()) {
+			await client.stop();
+		}
+		clients.clear();
 	}
 
 	const gofs = await lookpath("gofs");
@@ -28,47 +28,47 @@ export async function activate() {
 		return;
 	}
 
-	 // Find all go.mod files in the workspace
+	// Find all go.mod files in the workspace
 	const goModFiles = await vscode.workspace.findFiles('**/go.mod');
 	if (goModFiles.length === 0) {
 		vscode.window.showErrorMessage("No Go modules found in the workspace.");
 		return;
 	}
-	
+
 	for (const goModFile of goModFiles) {
 		const moduleDir = vscode.Uri.joinPath(goModFile, '..').fsPath;
 
 		const serverOptions: ServerOptions = {
-            run: {
-                command: gofs,
-                transport: TransportKind.stdio,
-                args: ["lsp"]
-            },
-            debug: {
-                command: gofs,
-                transport: TransportKind.stdio,
-                args: ["lsp", "--debug"]
-            }
-        };
+			run: {
+				command: gofs,
+				transport: TransportKind.stdio,
+				args: ["lsp"]
+			},
+			debug: {
+				command: gofs,
+				transport: TransportKind.stdio,
+				args: ["lsp", "--debug"]
+			}
+		};
 
 		const clientOptions: LanguageClientOptions = {
-            documentSelector: [{ scheme: 'file', pattern: `${moduleDir}/**/*.{templ,go}` }],
-            workspaceFolder: { uri: vscode.Uri.file(moduleDir), name: moduleDir, index: 0 }
-        };
+			documentSelector: [{ scheme: 'file', pattern: `${moduleDir}/**/*.{templ,go}` }],
+			workspaceFolder: { uri: vscode.Uri.file(moduleDir), name: moduleDir, index: 0 }
+		};
 
-        const client = new LanguageClient(
-            `gofs-${moduleDir}`,
-            `gofs (${moduleDir})`,
-            serverOptions,
-            clientOptions
-        );
+		const client = new LanguageClient(
+			`gofs-${moduleDir}`,
+			`gofs (${moduleDir})`,
+			serverOptions,
+			clientOptions
+		);
 
-        client.start();
-        clients.set(moduleDir, client);
+		client.start();
+		clients.set(moduleDir, client);
 	}
 }
 
 export function deactivate(): Thenable<void> | undefined {
 	const stopPromises = Array.from(clients.values()).map(client => client.stop());
-    return Promise.all(stopPromises).then(() => undefined);
+	return Promise.all(stopPromises).then(() => undefined);
 }
